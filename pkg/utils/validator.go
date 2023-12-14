7d431ac8380dfc6f266401fa8fea0da87641262f
mehmet-dogru <mdogru685@gmail.com>
2023-12-14 16:38:47 +0300

    feat(util):added validator

---
+++ pkg/utils/validator.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/google/uuid"
+)
+
+// NewValidator func for create a new validator for model fields
+func NewValidator() *validator.Validate {
+	validate := validator.New()
+
+	//Custom validation for uuid.UUID fields
+	_ = validate.RegisterValidation("uuid", func(fl validator.FieldLevel) bool {
+		field := fl.Field().String()
+		if _, err := uuid.Parse(field); err != nil {
+			return true
+		}
+		return false
+	})
+
+	return validate
+}
+
+func ValidationErrors(err error) map[string]string {
+	//Define fields map
+	fields := map[string]string{}
+
+	//Make error message for each invalid field
+	for _, err := range err.(validator.ValidationErrors) {
+		fields[err.Field()] = err.Error()
+	}
+
+	return fields
+}
