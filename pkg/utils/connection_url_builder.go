88a4fface250c5add3dd30034640fb51717dffa5
mehmet-dogru <mdogru685@gmail.com>
2023-12-15 11:26:55 +0300

    fix: connection url builder func name

--- pkg/utils/connection_url_builder.go
+++ pkg/utils/connection_url_builder.go
@@ -5,47 +5,53 @@ import (
 	"os"
 )
 
-func ConnectionUrlBuilder(n string) (string, error) {
+// ConnectionURLBuilder func for building URL connection.
+func ConnectionURLBuilder(n string) (string, error) {
+	// Define URL to connection.
 	var url string
 
+	// Switch given names.
 	switch n {
 	case "postgres":
-		//URL for PostgreSQL connection
+		// URL for PostgreSQL connection.
 		url = fmt.Sprintf(
-			"host=%s post=%s user=%s password=%s dbname=%s sslmode=%s",
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 			os.Getenv("DB_HOST"),
 			os.Getenv("DB_PORT"),
 			os.Getenv("DB_USER"),
 			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
 			os.Getenv("DB_SSL_MODE"),
 		)
 	case "mysql":
-		//URL for MySQL connection
+		// URL for Mysql connection.
 		url = fmt.Sprintf(
 			"%s:%s@tcp(%s:%s)/%s",
 			os.Getenv("DB_USER"),
 			os.Getenv("DB_PASSWORD"),
 			os.Getenv("DB_HOST"),
-			os.Getenv("DB_POST"),
+			os.Getenv("DB_PORT"),
 			os.Getenv("DB_NAME"),
 		)
 	case "redis":
-		//URL for Redis connection
+		// URL for Redis connection.
 		url = fmt.Sprintf(
 			"%s:%s",
 			os.Getenv("REDIS_HOST"),
 			os.Getenv("REDIS_PORT"),
 		)
 	case "fiber":
-		//URL for Fiber connection
+		// URL for Fiber connection.
 		url = fmt.Sprintf(
 			"%s:%s",
 			os.Getenv("SERVER_HOST"),
 			os.Getenv("SERVER_PORT"),
 		)
 	default:
+		// Return error message.
 		return "", fmt.Errorf("connection name '%v' is not supported", n)
 	}
 
+	// Return connection URL.
 	return url, nil
 }
