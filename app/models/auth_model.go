f8d16db0efbb46696b4f1882cf23e2e62eb99620
mehmet-dogru <mdogru685@gmail.com>
2023-12-15 10:57:33 +0300

    fix: model validate trim

--- app/models/auth_model.go
+++ app/models/auth_model.go
@@ -2,13 +2,13 @@ package models
 
 // SignUp struct to describe register a new user
 type SignUp struct {
-	Email    string `json:"email" validate:"required, email, lte=255"`
-	Password string `json:"password" validate:"required, lte=255"`
-	UserRole string `json:"user_role" validate:"required, lte=25"`
+	Email    string `json:"email" validate:"required,email,lte=255"`
+	Password string `json:"password" validate:"required,lte=255"`
+	UserRole string `json:"user_role" validate:"required,lte=25"`
 }
 
 // SignIn struct to describe register a new user
 type SignIn struct {
-	Email    string `json:"email" validate:"required, email, lte=255"`
-	Password string `json:"password" validate:"required, lte=255"`
+	Email    string `json:"email" validate:"required,email,lte=255"`
+	Password string `json:"password" validate:"required,lte=255"`
 }
